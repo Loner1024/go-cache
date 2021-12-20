53128cb5c5c493ff800f0192f06097f16e66ff6a
解添昊 <xietianhao@pudutech.com>
2021-12-20 13:59:30 +0800

    lru cache

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Loner1024/gocache
+
+go 1.17
---
+++ lru/lru.go
@@ -0,0 +1,80 @@
+package lru
+
+import (
+	"container/list"
+)
+
+type Cache struct {
+	maxBytes  int64 // 允许最大使用内存
+	nbytes    int64 // 当前已使用内存
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value) // 记录被删除时的回调函数, 可选
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(key string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// Get look ups a key's value
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest remove the oldest item (队首元素)
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to cache
+func (c *Cache) Add(key string, value Value) {
+	// 如果缓存已经存在, 就更新
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		// 缓存不存在, 新增缓存
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	// 超过最大缓存大小, 移除队首元素
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
